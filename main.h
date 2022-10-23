e82a78bc2de6547fe23cf4f7f39838221882b86f
winnie wahome <wahomewinn00@gmail.com>
2022-10-22 21:01:37 -0700

    printf

--- main.h
+++ main.h
@@ -1,34 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <limits.h>
-
 /**
-* struct op - flag / function object
-* @c: flag
-* @f: function
-*/
-
-typedef struct op
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
 {
-	char *c;
+	char *t;
 	int (*f)(va_list);
-} op_t;
+} print_t;
 
-int _printf(const char *format, ...);
 int _putchar(char c);
-int print_c(va_list ar_list);
-int (*get_func(char s))(va_list ar_list);
-int print_s(va_list ar_list);
-int print_d(va_list ar_list);
-int print_i(va_list ar_numlist);
-int print_b(va_list binary_list);
-int print_u(va_list ar_list);
-int print_o(va_list ar_list);
-int print_x(va_list ar_list);
-int print_X(va_list ar_list);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif
+#endif  /* _MAIN_H */
